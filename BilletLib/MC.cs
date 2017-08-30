2130d60f2659e68e01f7ee169b4e5e48172bf36b
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-08-30 12:24:53 +0200

    Oprettet MC class so inkludree Props og Metoder Samt Unit Tests over MC's Metoder.

---
+++ BilletLib/MC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib
+{
+    public class MC
+    {
+        public string NummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public int Pris()
+        {
+            int total = 125;
+            return total;
+        }
+
+        public string KøreTøj()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BilletLibTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            //arrange
+            var mc = new MC();
+            //act
+            int pris = mc.Pris();
+            //assert
+            Assert.AreEqual(125,pris);
+        }
+
+        [TestMethod()]
+        public void KøreTøjTest()
+        {
+            //arrange
+            var mc = new MC();
+            //act
+            string køretøj = mc.KøreTøj();
+            //assert
+            Assert.AreEqual("MC", køretøj);
+        }
+    }
+}
