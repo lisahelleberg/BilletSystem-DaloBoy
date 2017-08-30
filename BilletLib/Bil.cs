6849c37914a7b49aff40a93fade26b23ee23d5e4
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-08-30 12:20:06 +0200

    Oprettet Bil Klasse samt t props, og 2 metoder. Inkludere Unit Tests over de to metoder i Bil klassen

---
+++ BilletLib/Bil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib
+{
+    public class Bil
+    {
+        public string nummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public int Pris()
+        {
+            int fastPris = 240;
+            return fastPris;
+        }
+
+        public string køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ BilletLibTests/BilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            //arrange
+            var bil = new Bil();
+            //act
+            int pris = bil.Pris();
+            //assert
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod()]
+        public void køretøjTest()
+        {
+            //arrange
+            var bil = new Bil();
+            //act
+            string køretøj = bil.køretøj();
+            //assert
+            Assert.AreEqual("Bil", køretøj);
+        }
+    }
+}
